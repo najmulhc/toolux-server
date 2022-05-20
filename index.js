14a3a8cd91c511a17eb84038441973946113608e
kridi32 <kridi32@outlook.com>
2022-05-20 16:50:55 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// all the http requests wil be here 
+app.get("/" , (req, res) => {
+    res.send("welcome to the node server!")
+})
+
+app.listen(port, () => {
+  console.log("node server is started");
+});
